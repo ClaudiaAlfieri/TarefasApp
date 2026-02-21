1f4a9788427ac36c0102c331e7cba6b39081d9d8
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-02-21 21:46:09 +0000

    Create Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,65 @@
+import { useState } from 'react'
+import './home.css'
+
+import {Link} from 'react-router-dom'
+
+import { auth} from '../../firebaseConection'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+import {useNavigate} from 'react-router-dom'
+
+
 export default function Home(){
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate();
+
+  async function handleLogin(e){
+    e.preventDefault();
+    if(email !== '' && password !== ''){
+      await signInWithEmailAndPassword(auth, email, password)
+      .then(()=>{
+        //navegar para admin
+        navigate('/admin', {replace: true})
+      })
+      .catch(()=>{
+        alert("Falha ao fazer login")
+      })
+    }else{
+      alert("Por favor preencha todos os campos")
+    }
+
+  }
+
+
   return(
-    <div>
-    <h1>Página Home</h1>
+    <div className="home-container">
+    <h1>Lista de tarefas</h1>
+    <span>Gerencie sua agenda de forma fácil.</span>
+
+    <form className="form" onSubmit={handleLogin}>
+      <input
+      type="text"
+      placeholder="Digite seu email"
+      value={email}
+      onChange={ (e) => setEmail(e.target.value)}
+      />
+
+       <input
+      type="password"
+      placeholder="Digite sua senha"
+      value={password}
+      onChange={ (e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Acessar</button>
+    </form>
+
+      <Link className="button-link" to="./register">
+      Não possui uma conta? Cadastre-se
+      </Link>
+
     </div>
   )
 }
